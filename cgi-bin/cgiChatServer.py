c3d404489e1105c5be11a25cc397e408c6c35e41
Pazitos10 <pazosbruno@hotmail.com>
2014-10-23 11:56:04 -0300

    Se añade funcionalidad de chat (básico) y archivos relacionados

---
+++ cgi-bin/cgiChatServer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import os, fcntl
+from ej4 import parsear, limpiar
+
+CHATFNAME = "chat.txt"
+MAXLINE = 500
+
+def main():
+
+    print "Content-type: text/plain\n\n"
+    print ""
+
+    print "<html><head></head><body>"
+    
+    query_string = os.getenv('QUERY_STRING')
+    chatfile = open(CHATFNAME,"a+")
+    if not chatfile:
+        print "<p>Error abriendo el archivo<br></p>\n"
+
+    fd = chatfile.fileno()
+    try:
+        fcntl.flock(fd, fcntl.LOCK_EX|fcntl.LOCK_NB) #Bloqueamos el archivo
+    except IOError:
+        print "<p> Error en flock()</p>\n"
+    
+    for l in chatfile.readlines():
+        print l
+
+    news = limpiar(parsear(query_string)) #Lo nuevo es lo parseado de query_string
+    news = news['newtxt'] #obtenemos el valor de adentro del diccionario
+    chatfile.write(news)
+    fcntl.flock(fd, fcntl.LOCK_UN) #Desbloqueamos el archivo
+    chatfile.close()
+    print "<p>%s</p>" % (news)
+    print "</body></html>"
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ statics/js/chat.js
@@ -0,0 +1,28 @@
+function getReqObj() {
+    var XMLHttpRequestObject; 
+        if (window.XMLHttpRequest) { 
+            XMLHttpRequestObject = new XMLHttpRequest(); 
+        } else if (window.ActiveXObject) { 
+            XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP"); 
+        }
+        return XMLHttpRequestObject; 
+}
+//---------------------------------------------------------------
+function submitHandler(txtID, divID) 
+{
+    var theText = document.getElementById(txtID);
+    var theArea = document.getElementById(divID);
+    var reqObj = getReqObj();
+    var url="../cgi-bin/cgiChatServer.py?newtxt=" + escape("<p>" + theText.value + "</p>");
+    reqObj.open("GET", url, false);
+    reqObj.send(null); 
+    theArea.innerHTML = unescape(reqObj.responseText);
+    theArea.scrollTop = theArea.scrollHeight;
+    theText.value = "";
+    return false;
+}
+//---------------------------------------------------------------
+window.onload=function()
+{
+    document.forms[0][0].focus();
+} 
